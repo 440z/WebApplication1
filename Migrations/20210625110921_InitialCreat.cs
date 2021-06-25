c2133c0e630565ca172c01bd75785f520bf57603
440z <hisdudeness777@web.de>
2021-06-25 13:14:21 +0200

    ConnectionString passt und funktioniert

---
+++ Migrations/20210625110921_InitialCreat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class InitialCreat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Length = table.Column<int>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    PublishingDate = table.Column<DateTime>(nullable: false),
+                    DataType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Document");
+        }
+    }
+}
